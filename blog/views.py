dcb827f191d1c24bee110b5d7f3d21a273cd3d25
Jasmeen <jas@mail.com>
2019-06-25 17:06:43 +0530

    Views Created

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from .models import Post,Comment
+from .forms import PostForm,CommentForm
+from django.urls import reverse_lazy
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (TemplateView,ListView,
+                                  DetailView,CreateView,
+                                  UpdateView,DeleteView)
 
 # Create your views here.
+
+class About(TemplateView):
+    template_name='blog/about.html'
+
+class PostListView(ListView):
+    model=Post
+
+    def get_queryset(self):
+        return Post.objects.filter(publish_date__lte=timezone.now()).order_by('-publish_date')
+
+class PostDetailView(DeleteView):
+    model=Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    login_url='/login/'
+    redirect_field_name='blog/post_detail.html'
+    model=Post
+    form_class= PostForm
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url='/login/'
+    redirect_field_name='blog/post_detail.html'
+    model=Post
+    form_class= PostForm
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model=Post
+    success_url=reverse_lazy('post_list')
+
+class DraftListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(publish_date__isnull=True).order_by('create_date')       
